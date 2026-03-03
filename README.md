5b261772138075bfd4d5aed8873613df556a21e3
Alexander Tan <73046004+axtcsq@users.noreply.github.com>
2026-03-03 17:32:07 +0800

    Update 1: Added and cleaned up files, and created a basic sign up & login page to get started.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# G11T3
-New GitHub Repository
+# WAD1 Project (G11 Team 3)
+
+## About this Project
+This pet adoption platform website is a dynamic and data-driven web application that leverages the core back-end
+technologies taught in IS113: Node.js, Express.js, and MongoDB. This project requires us to design and
+implement a functioning back-end using routes, connect to a database, and render user-facing pages
+dynamically with authentication and security features.
+
+## Student Names
+* Ryan Lim Rui Jun
+* Ng Zi Ling Pearl
+* Alexander Tan
+* Goh Kay Herng
+* Lim Yi Wei
+* Irwin Sim
+
+## Instructions
+*To use our project, please follow these sets of instructions:*
+### a. How to set up our application based on the submitted file(s)
+* x
+
+### b. How to run our application
+* x
+
+### c. Username/password details used
+* x
--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-const express = require('express')
-const path = require('path')
+// Required Codes
+const express = require("express");
 const server = express();
 
-server.use("/", express.static(path.join(__dirname, "public")))
+// POST
 server.use(express.urlencoded({ extended: true }));
-const hostname = 'localhost'
-const port = 8000
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-    console.log(`dirname: ${__dirname}`)
+// STATIC FILES
+const path = require("path");
+server.use("/", express.static(path.join(__dirname, "public")));
+
+// VIEW ENGINE
+server.set("view engine", "ejs");
+
+// START Code
+server.get("/", (req, res) => {
+    res.redirect("/index.html");
 })
+
+server.post("/login-process", (req, res) => {
+  const data = req.body;
+  const username = data.username;
+  const password = data.password;
+  const cfmPassword = data.cfmPassword;
+
+  res.send("Signed up!");
+})
+
+server.post("/signup-process", (req, res) => {
+  const data = req.body;
+  const username = data.username;
+  const password = data.password;
+
+  res.send("Login!");
+})
+
+// END Code
+
+// Define the hostname and port for the server
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Start server and listen for incoming requests
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
