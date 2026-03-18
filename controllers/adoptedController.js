4d464b79ef1aebee587b8dbb146fb839f65ffbf9
Yiwei1407 <yiwei.lim.2025@computing.smu.edu.sg>
2026-03-18 15:37:04 +0800

    yiwei changed
    yiwei changed

--- controllers/adoptedController.js
+++ controllers/adoptedController.js
@@ -13,14 +13,24 @@ exports.displayAdopted = async (req, res) => {
 
   try {
 
+    // 1. perform the database actions 
     //delete pet
     await Pet.delPet(id)
-
     //add adopted
     await adopted.addAdopted(newAdopted)
 
+    // 2. FETCH the pet we just added to show it on the summary page
+      // Assuming your adopted model has a way to get all adopted pets
+      let adoptedList = await adopted.retrieveAll(); 
+      
+      // Grab the most recent one (the one we just added)
+      const lastPet = adoptedList[adoptedList.length - 1];
 
-    res.render("adopted-pets",{isAdmin}); // Render the EJS form view and pass the posts
+      // 3. PASS 'pet' to the EJS
+      res.render("adopted-pets", {
+          isAdmin: isAdmin,
+          pet: lastPet // This is the key! This stops the "pet is undefined" error
+      });
 
   } catch (error) {
     console.error(error);
