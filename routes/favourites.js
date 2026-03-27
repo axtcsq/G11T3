9b3c0c4f78b834510b0173e49bfa33b98d3e5cfe
Alexander Tan <73046004+axtcsq@users.noreply.github.com>
2026-03-27 22:36:02 +0800

    Minor: Added route protection for favourites

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { requireLogin, requireAdmin } = require("../middleware/auth");
+
 const favouritesController = require("../controllers/favouritesController")
 
-router.get("/add-favourite", favouritesController.addFavourite)
-router.get("/remove-favourite", favouritesController.removeFavourite)
-router.get("/view-favourites", favouritesController.showFavourites)
-router.get("/delete-favourite", favouritesController.deleteFavourite)
-router.get("/edit-favourite", favouritesController.editFavourite);
-router.post("/update-favourite", favouritesController.updateFavourite);
+router.get("/add-favourite", requireLogin, favouritesController.addFavourite)
+router.get("/remove-favourite", requireLogin, favouritesController.removeFavourite)
+
+router.get("/view-favourites", requireLogin, favouritesController.showFavourites)
+router.get("/delete-favourite", requireLogin, favouritesController.deleteFavourite)
+
+router.get("/edit-favourite", requireLogin, favouritesController.editFavourite);
+router.post("/update-favourite", requireLogin, favouritesController.updateFavourite);
 
 module.exports = router
