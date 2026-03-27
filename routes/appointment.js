aa49dbe89528cbca01db14231e0507b50f19d8a8
Alexander Tan <73046004+axtcsq@users.noreply.github.com>
2026-03-27 22:56:24 +0800

    8.5: Updated design for reviews & implemented route protection for appointments

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const { requireLogin, requireAdmin } = require("../middleware/auth");
 const router = express.Router();
+
 const appointmentController = require("../controllers/appointmentController");
 
 // The routes tell the router which "brain" (controller) to wake up to handle each specific request.
 
 // route for CREATE
-router.post('/book', appointmentController.bookAppointment);
+router.post('/book', requireLogin, appointmentController.bookAppointment);
 
 // route for READ
-router.get('/view-appointment', appointmentController.viewAppointments);
+router.get('/view-appointment', requireLogin, appointmentController.viewAppointments);
 
 // routes for UPDATE 
 // GET shows the form
-router.get('/edit-appointment/:id', appointmentController.editAppointmentPage);
+router.get('/edit-appointment/:id', requireLogin, appointmentController.editAppointmentPage);
 // POST saves the data
-router.post('/update-appointment/:id', appointmentController.updateAppointment);
+router.post('/update-appointment/:id', requireLogin, appointmentController.updateAppointment);
 
 // route for DELETE 
 // The :id is a placeholder. It tells the server: "If someone visits a URL that starts with /delete/, take whatever characters come next and call them id."
-router.get('/delete-appointment/:id', appointmentController.deleteAppointment);
+router.get('/delete-appointment/:id', requireLogin, appointmentController.deleteAppointment);
 
 
 module.exports = router;
